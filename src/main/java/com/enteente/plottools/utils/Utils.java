d1859933812aa14e87efc2f6d4d39bbaa5005085
schubc <schubc@enteente.com>
2019-10-06 11:57:49 +0200

    code cleanup

--- src/main/java/com/enteente/plottools/utils/Utils.java
+++ src/main/java/com/enteente/plottools/utils/Utils.java
@@ -20,65 +20,71 @@ import com.github.intellectualsites.plotsquared.plot.util.PlotGameMode;
 import com.github.intellectualsites.plotsquared.plot.util.UUIDHandler;
 
 public final class Utils {
+
+	private Utils() {
+		throw new IllegalStateException("Utility class");
+	}
+
 	public static int countPlots(Player player) {
-        PlotPlayer pp=PlotPlayer.wrap(player);
-        String world=Plottools.getInstance().getConfig().getString("world");
-        Set<Plot> plots=pp.getPlots();
-        List<Plot> plist = new ArrayList<Plot>(plots);
-        int plotCount=0;
-        for(Plot p:plist) {
-        	if(p.getWorldName().equalsIgnoreCase(world)) {
-        		plotCount++;
-        	}
-        }
-		
+		PlotPlayer pp = PlotPlayer.wrap(player);
+		String world = Plottools.getInstance().getConfig().getString("world");
+		Set<Plot> plots = pp.getPlots();
+		List<Plot> plist = new ArrayList<>(plots);
+		int plotCount = 0;
+		for (Plot p : plist) {
+			if (p.getWorldName().equalsIgnoreCase(world)) {
+				plotCount++;
+			}
+		}
+
 		return plotCount;
 	}
+
 	public static double getPrice(int n) {
 
-		double b=Plottools.getInstance().getConfigs().get().getDouble("baseprice");
-		double e=Plottools.getInstance().getConfigs().get().getDouble("exponent");
-		int r=Plottools.getInstance().getConfigs().get().getInt("round");
-		
-		double p=b*Math.pow(e, n);
-		if(r!=0) {
-			p=r*(double)Math.round(p/r);
+		double b = Plottools.getInstance().getConfigs().get().getDouble("baseprice");
+		double e = Plottools.getInstance().getConfigs().get().getDouble("exponent");
+		int r = Plottools.getInstance().getConfigs().get().getInt("round");
+
+		double p = b * Math.pow(e, n);
+		if (r != 0) {
+			p = r * (double) Math.round(p / r);
 		}
-		
+
 		return p;
 	}
-    public static void getPersistentMeta(UUID uuid, final String key,
-            final RunnableVal<byte[]> result) {
-            PlotPlayer player = UUIDHandler.getPlayer(uuid);
-            if (player != null) {
-                result.run(player.getPersistentMeta(key));
-            } else {
-                DBFunc.getPersistentMeta(uuid, new RunnableVal<Map<String, byte[]>>() {
-                    @Override public void run(Map<String, byte[]> value) {
-                        result.run(value.get(key));
-                    }
-            	});
-            }
-        }
-    
-    public static Location getLocationFull(final Entity entity) {
-        final org.bukkit.Location location = entity.getLocation();
-        return new Location(location.getWorld().getName(), MathMan.roundInt(location.getX()),
-            MathMan.roundInt(location.getY()), MathMan.roundInt(location.getZ()), location.getYaw(),
-            location.getPitch());
-    }
-    
-    public static void fixFlight(Plot origin) {
-        for (Plot plot : origin.getConnectedPlots()) {
-            for (PlotPlayer pp : plot.getPlayersInPlot()) {
-                boolean flight = pp.getFlight();
-                PlotGameMode gamemode = pp.getGameMode();
-                if(gamemode != PlotGameMode.CREATIVE && gamemode != PlotGameMode.SPECTATOR) {
-                	pp.setFlight(false);
-                }
 
-            }
-        }    	
-    }
-    
+	public static void getPersistentMeta(UUID uuid, final String key, final RunnableVal<byte[]> result) {
+		PlotPlayer player = UUIDHandler.getPlayer(uuid);
+		if (player != null) {
+			result.run(player.getPersistentMeta(key));
+		} else {
+			DBFunc.getPersistentMeta(uuid, new RunnableVal<Map<String, byte[]>>() {
+				@Override
+				public void run(Map<String, byte[]> value) {
+					result.run(value.get(key));
+				}
+			});
+		}
+	}
+
+	public static Location getLocationFull(final Entity entity) {
+		final org.bukkit.Location location = entity.getLocation();
+		return new Location(location.getWorld().getName(), MathMan.roundInt(location.getX()),
+				MathMan.roundInt(location.getY()), MathMan.roundInt(location.getZ()), location.getYaw(),
+				location.getPitch());
+	}
+
+	public static void fixFlight(Plot origin) {
+		for (Plot plot : origin.getConnectedPlots()) {
+			for (PlotPlayer pp : plot.getPlayersInPlot()) {
+				PlotGameMode gamemode = pp.getGameMode();
+				if (gamemode != PlotGameMode.CREATIVE && gamemode != PlotGameMode.SPECTATOR) {
+					pp.setFlight(false);
+				}
+
+			}
+		}
+	}
+
 }
